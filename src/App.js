3aa8361a998acd334ae7b18ffc435276fea63b93
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-20 16:52:55 -0800

    add content to test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div 
-      className="App"
-    >
+    <div className="App">
+        <div 
+          className="App__products">
+            Imagine your brand here Imagine your brand here Imagine your brand here Imagine your brand here  
+            Imagine your brand here Imagine your brand here Imagine your brand here
+            <p>Photo by Rodion Kutsaiev on Pexels</p>
+        </div>
+        <div
+          className="App__specs"
+        > Imagine your product specs here Imagine your product specs here 
+        Imagine your product specs here Imagine your product specs here Imagine your product specs here </div>
       
-    <p>Photo by Rodion Kutsaiev on Pexels</p>
+    
     </div>
   );
 }
