cda1b58b6d64dfbb35d1cf8480f912c09e1a8eee
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-20 17:01:41 -0800

    adjust the widths

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ function App() {
     <div className="App">
         <div 
           className="App__products">
-            Imagine your brand here Imagine your brand here Imagine your brand here Imagine your brand here  
-            Imagine your brand here Imagine your brand here Imagine your brand here
+            <h1>Imagine your brand here</h1> 
+            <p>Imagine your brand here Imagine your brand here Imagine your brand here  
+            Imagine your brand here Imagine your brand here Imagine your brand here</p>
             <p>Photo by Rodion Kutsaiev on Pexels</p>
         </div>
         <div
           className="App__specs"
-        > Imagine your product specs here Imagine your product specs here 
-        Imagine your product specs here Imagine your product specs here Imagine your product specs here </div>
+        > 
+        <p>Imagine your product specs here Imagine your product specs here 
+        Imagine your product specs here Imagine your product specs here Imagine your product specs here 
+        </p>
+        </div>
       
     
     </div>
